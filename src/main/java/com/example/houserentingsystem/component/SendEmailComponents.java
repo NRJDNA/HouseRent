d805f45d11efb584a8e14f262801ae23a3633add
NRJDNA <nirajandangalchanpabot@gmail.com>
2022-09-01 10:44:42 +0545

    email component

--- src/main/java/com/example/houserentingsystem/component/SendEmailComponents.java
+++ src/main/java/com/example/houserentingsystem/component/SendEmailComponents.java
@@ -1,32 +1,29 @@
 package com.example.houserentingsystem.component;
 
-import org.apache.commons.mail.DefaultAuthenticator;
-import org.apache.commons.mail.Email;
-import org.apache.commons.mail.SimpleEmail;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
 /**
  * @author dangal_nirajan on 29/08/2022
  */
-@Component
-public class SendEmailComponents {
-    public void sendEmail(String emailAddress, String Subject, String message) {
-        try {
-            Email email = new SimpleEmail();
-            email.setHostName("smtp.googlemail.com");
-            email.setSmtpPort(465);
-            email.setAuthentication("copycon2022@gmail.com","copycon1234");
-            email.setSSL(true);
-            email.setFrom("copycon2022@gmail.com");
-            email.setSubject("Registration");
-            email.setMsg(message);
-            email.addTo(emailAddress);
-            email.send();
-            System.out.println("Sent");
+@Service
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Failed");
-        }
+public class SendEmailComponents {
+    @Autowired
+    private JavaMailSender mailSender;
+    public void sendEmail(String toEmail, String subject, String body) {
+
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom("copycon2022@gmail.com");
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+
+        mailSender.send(message);
+
+        System.out.println("Mail sent Successfully");
     }
 }
